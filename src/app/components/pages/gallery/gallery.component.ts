c952039157b53b8a7b50080fc4291712318b8810
azizlf <developer.mohamedazizlouifi@gmail.com>
2023-02-10 01:48:55 +0100

    update design activité v2

--- src/app/components/pages/gallery/gallery.component.ts
+++ src/app/components/pages/gallery/gallery.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as  AOS from 'aos';
+import Swiper from "swiper";
 
 
 @Component({
@@ -14,6 +15,30 @@ export class GalleryComponent implements OnInit {
     AOS.init();
 
   }
+
+  cover_restau_page_images = [
+    {
+        "id": 1,
+        "img": "https://d1vp8nomjxwyf1.cloudfront.net/wp-content/uploads/sites/364/2021/04/01101407/Restaurant-HotelGRandAmour-%40PionPhotographie-4.jpg"
+       
+    },
+    {
+        "id": 2,
+        "img": "https://tunisie.co/uploads/images/content/trt-210622-1.jpg"
+    },
+    {
+        "id": 3,
+        "img": "https://blog.cap-adrenaline.com/wp-content/uploads/2022/03/equipement-cours-equitation.jpg"
+    },
+    {
+        "id": 4,
+        "img": "assets/img/aboutcarousel/03.jpg"
+    },
+    {
+        "id": 5,
+        "img": "https://www.fribourgregion.ch/wp-content/uploads/2022/03/randonnee_hb.jpg"
+    }
+  ]
 
   actsList:any = [
 
@@ -69,7 +94,53 @@ export class GalleryComponent implements OnInit {
 
   ]
 
+  containerSliderRestau:any
+
+  createCoverSliderRestau(slide:any){
+
+    this.containerSliderRestau = document.getElementById("containerSliderRestau")
+
+    var container = document.createElement("div")
+    
+    container.setAttribute("class","swiper-slide")
+
+    container.setAttribute("style",`
+
+      width: 100%;
+      height: 100%;
+      background-size: cover;
+      background-position: center;
+      background-image: url(${slide.img});
+
+    `)
+
+    this.containerSliderRestau.appendChild(container)
+
+  }
+
+  initRestausSlider(){
+    new Swiper(".restau-slider", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      loop:true,
+      navigation:{
+
+        nextEl:".slide-restau-right",
+        prevEl:".slide-restau-left"
+
+      }
+    })
+  }
+
   ngOnInit(): void {
+
+    this.cover_restau_page_images.map((img:any)=>{
+
+      this.createCoverSliderRestau(img)
+
+    })
+
+    this.initRestausSlider()
   }
 
 }
