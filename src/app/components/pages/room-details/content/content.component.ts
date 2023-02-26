6d102bde9e29b8bb2bbc4ff4130d93ec12df2fe2
azizlf <developer.mohamedazizlouifi@gmail.com>
2023-02-26 13:02:03 +0100

    update design v10 3

--- src/app/components/pages/room-details/content/content.component.ts
+++ src/app/components/pages/room-details/content/content.component.ts
@@ -337,6 +337,28 @@ settings = {
 
   }
 
+  initPricesSlider(){
+
+    new Swiper(".tranding-slider", {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      loop: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2.5,
+      },
+      navigation: {
+        nextEl: '.slide-right',
+        prevEl: '.slide-left',
+      }
+    })
+
+  }
+
 
 
 /************************************* */
@@ -393,6 +415,8 @@ settings = {
     })
 
     this.initRoomDetailsSlider()
+
+    this.initPricesSlider()
 
   }
 
